a23843ad6c38224bb64ed14a1a72fb169f741c58
TakumiKawahara <64956716+TakumiKawahara@users.noreply.github.com>
2020-07-06 19:59:45 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  # before_action :set_post, only: [:show]
   
   def index
     @posts = Post.all
@@ -9,12 +10,21 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    render 'index'
+    @post = Post.create(post_params)
+    
+    redirect_to new_post_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :text)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :posts, only: [:index, :new, :create]
+
+  resources :posts, only: [:index, :new, :create, :show]
+
 end
